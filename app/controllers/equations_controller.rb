e30a5fcaa651d2489300c1736e714f36785cd1a6
tarrinros <tarindis@gmail.com>
2019-09-12 20:35:43 +0300

    Add equation partial

--- app/controllers/equations_controller.rb
+++ app/controllers/equations_controller.rb
@@ -1,19 +1,21 @@
 class EquationsController < ApplicationController
   before_action :authenticate_user!, except: [:index]
-
   before_action :set_current_user, except: [:index]
+  before_action :set_equation, only: [:show]
 
   def index
     @equations = Equation.all
   end
 
-  def show
-    @equation = Equation.find_by(params[:id])
-  end
+  def show; end
 
   def result; end
 
   private
+
+  def set_equation
+    @equation = Equation.find_by(params[:id])
+  end
 
   def set_current_user
     @user = current_user
