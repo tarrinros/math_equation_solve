e8b17220ffb9c55c7228bfd8fcca71f4cc56fe02
tarrinros <tarindis@gmail.com>
2019-09-12 13:58:07 +0300

    Add registrations controller

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  devise :database_authenticatable, validatable,
+  devise :database_authenticatable, :validatable,
          :jwt_authenticatable,
          jwt_revocation_strategy: JWTBlacklist
 end
