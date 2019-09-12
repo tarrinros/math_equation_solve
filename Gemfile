e28f509ee93624bf05e18273f7ddbffa058b68f6
tarrinros <tarindis@gmail.com>
2019-09-12 16:11:09 +0300

    Add devise for authentication

--- Gemfile
+++ Gemfile
@@ -7,6 +7,7 @@ gem 'bootstrap', '~> 4.2.1'
 gem 'jquery-rails'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.2.3'
+gem 'devise'
 # Use sqlite3 as the database for Active Record
 gem 'sqlite3'
 # Use Puma as the app server
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  def configure_permitted_parameters
+    # .for method was changed by the .permit method for devise version >= 4
+    devise_parameter_sanitizer.permit(:account_update) { |u|
+      u.permit(:password, :password_confirmation, :current_password)
+    }
+  end
 end
