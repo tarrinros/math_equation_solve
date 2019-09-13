91c542cc322dd1f321778cb8f171c484f01eef50
tarrinros <tarindis@gmail.com>
2019-09-13 22:19:07 +0300

    Add specs for equation controller

--- spec/controllers/equation_controller_spec.rb
+++ spec/controllers/equation_controller_spec.rb
@@ -1,5 +1,46 @@
 require 'rails_helper'
 
-RSpec.describe EquationController, type: :controller do
+RSpec.describe EquationsController, type: :controller do
+  let(:user) { FactoryBot.create :user }
+  let(:equation) { FactoryBot.create :equation }
 
+  describe '#index' do
+    it 'responds successfully' do
+      get :index
+      expect(response).to be_successful
+    end
+
+    it 'returns a 200 response' do
+      get :index
+      expect(response).to have_http_status '200'
+    end
+  end
+
+  describe '#show' do
+    context 'as an authorized user' do
+      before(:each) { sign_in user }
+
+      it 'responds successfully' do
+        get :show, params: { id: equation.id }
+        expect(response).to be_successful
+      end
+
+      it 'returns a 200 response' do
+        get :show, params: { id: equation.id }
+        expect(response).to have_http_status '200'
+      end
+    end
+
+    context 'guest' do
+      it 'returns a 302 response' do
+        get :show, params: { id: equation.id }
+        expect(response).to have_http_status '302'
+      end
+
+      it 'redirects to the sign-in page' do
+        get :show, params: { id: equation.id }
+        expect(response).to redirect_to '/users/sign_in'
+      end
+    end
+  end
 end
