22d784f92810dc8256157eb1cc1899d4c1703a77
tarrinros <tarindis@gmail.com>
2019-09-16 20:08:31 +0300

    Add linear simple equations solver

---
+++ app/services/linear_equation_solver.rb
@@ -0,0 +1,30 @@
+class LinearEquationSolver < ApplicationService
+  attr_reader :a, :b, :c
+
+  def initialize(factors)
+    @a = factors.fetch('a')
+    @b = factors.fetch('b')
+    @c = factors.fetch('c')
+  end
+
+  def call
+    return {text: 'a = 0. Equation cannot be solved'} if a.zero?
+    if b > 0
+      factor_b = -b
+      solve(factor_b)
+    else
+      factor_b = +b
+      solve(factor_b)
+    end
+  end
+
+  private
+
+  def solve(b)
+    result = (c + b) / a
+    return {
+      text: 'Solution:',
+      x: "#{result}"
+    }
+  end
+end
