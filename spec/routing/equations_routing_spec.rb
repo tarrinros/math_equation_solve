938bb23d78d33d4735abc11d7f58de66b8c5b9df
tarrinros <tarindis@gmail.com>
2019-09-16 22:38:20 +0300

    Add equations routing spec

--- spec/routing/equations_routing_spec.rb
+++ spec/routing/equations_routing_spec.rb
@@ -1,13 +1,17 @@
 require 'rails_helper'
 
-RSpec.describe LinksController, type: :routing do
+RSpec.describe EquationsController, type: :routing do
   describe 'routing' do
     it 'routes to #index' do
-      expect(:get => '/equations').to route_to('equations#index')
+      expect(get: '/equations').to route_to('equations#index')
     end
 
     it 'routes to #show' do
-      expect(:get => '/equations/1').to route_to('equations#show', id: '1')
+      expect(get: '/equations/1').to route_to('equations#show', id: '1')
+    end
+
+    it 'routes to #solve' do
+      expect(post: '/equations/solve').to route_to('equations#solve')
     end
   end
 end
